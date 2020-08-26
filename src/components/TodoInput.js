5c8bf0343c25130efc3c6908083dbf1c52d354f3
jojowabbit <jojowabbit@gmail.com>
2020-08-26 21:31:30 +0800

    created form in todoinput

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,7 +2,30 @@ import React, { Component } from "react";
 
 class TodoInput extends Component {
   render() {
-    return <div>TodoInput</div>;
+    const { item, editItem, handleChange, handleSubmit } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fas fa-book"></i>
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add todo"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="btn btn-block btn-primary mt-3 text-uppercase">
+            Add item
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
