0a102b28209b10291571b16af0bb631db4c89e90
jojowabbit <jojowabbit@gmail.com>
2020-08-26 21:53:03 +0800

    added font awesome icon, added handleChange & handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 class App extends Component {
   state = {
-    items: [
-      { id: 1, title: "todo 1" },
-      { id: 2, title: "todo 2" },
-    ],
+    items: [],
     id: uuidv4(),
     item: "",
     editItem: false,
   };
 
-  handleChange = (e) => console.log("handling change");
-  handleSubmit = (e) => console.log("handling submit");
+  handleChange = (e) => this.setState({ item: e.target.value });
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = { id: this.state.id, title: this.state.item };
+    const updatedItems = [...this.state.items, newItem];
+    // update & prepare state for next entry
+    this.setState(
+      {
+        items: updatedItems,
+        item: "",
+        id: uuidv4(),
+        editItem: false,
+      },
+      () => console.log(this.state)
+    );
+  };
   clearList = () => console.log("clearing list");
   handleDelete = (id) => console.log(`deleting todo ${id}`);
   handleEdit = (id) => console.log(`editing todo ${id}`);
