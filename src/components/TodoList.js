a42958f9c1007888b6b59fe99597c40d76c7eef0
jojowabbit <jojowabbit@gmail.com>
2020-08-26 22:07:21 +0800

    structured todolist component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,28 @@ import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <div>
-        <TodoItem />
-      </div>
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
+        {items.map((item) => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete}
+              handleEdit={() => handleEdit}
+            />
+          );
+        })}
+        <button
+          type="button"
+          className="btn btn-danger btn-block text-uppercase mt-5"
+          onClick={clearList}
+        >
+          Clear List
+        </button>
+      </ul>
     );
   }
 }
