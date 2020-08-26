a14632a1fa817bbe3f8f4ef7f96324e1a4a701c4
jojowabbit <jojowabbit@gmail.com>
2020-08-26 21:05:31 +0800

    app structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
+class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "todo 1" },
+      { id: 2, title: "todo 2" },
+    ],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => console.log("handling change");
+  handleSubmit = (e) => console.log("handling submit");
+  clearList = () => console.log("clearing list");
+  handleDelete = (id) => console.log(`deleting todo ${id}`);
+  handleEdit = (id) => console.log(`editing todo ${id}`);
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              editItem={this.state.editItem}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
+            <TodoList
+              items={this.state.items}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+              clearList={this.clearList}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class TodoInput extends Component {
+  render() {
+    return <div>TodoInput</div>;
+  }
+}
+
+export default TodoInput;
